17a6e586354278d24b304a2e7d8bdb4d11084364
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2025-12-23 20:24:25 +0000

    updated stuff

--- components/CombinedLogo.tsx
+++ components/CombinedLogo.tsx
@@ -1,19 +1,30 @@
-"use client"
-
 import Link from "next/link"
 import Image from "next/image"
-import { useTheme } from "next-themes"
 
 interface CombinedLogoProps {
   className?: string
 }
 
 export function CombinedLogo({ className = "" }: CombinedLogoProps) {
-  const { theme } = useTheme()
   return (
     <div className={`flex items-center gap-1 font-normal ${className}`}>
       <Link href="/" className="relative">
-        <Image src={theme === "dark" ? "/images/logos/logo-white.png" : "/images/logos/logo-black.png"} alt="reevit" width={100} height={100} />
+        <Image
+          src="/images/logos/logo-black.png"
+          alt="reevit"
+          width={100}
+          height={100}
+          className="dark:hidden"
+          priority
+        />
+        <Image
+          src="/images/logos/logo-white.png"
+          alt="reevit"
+          width={100}
+          height={100}
+          className="hidden dark:block"
+          priority
+        />
       </Link>
     </div>
   )
